f303b1c151cdd43bacb6f2a37adfc8facc62d5da
jjavierga <jjgabascal@gmail.com>
2021-07-21 10:36:26 +0200

    Union transductive-inductive

---
+++ scripts/evaluate/inductive/deffects_inductive.sh
@@ -0,0 +1,19 @@
+
+# ===========================> Resnet 18 <=====================================
+# Alfabetical order of classes [568,127,8,257,15,13,234]
+python3 -m src.mainV5 \
+		-F logs/tim_gd/mini/resnet18 \
+		with dataset.path="/media/grvc/MAMG_ViGUS/GRVC/Datasets/ML/Bridges_or_cracks/multiclassifier/MultiClassifier_csv/images_with_general_defects" \
+		ckpt_path="checkpoints/mini/softmax/resnet18" \
+		dataset.split_dir="/media/grvc/MAMG_ViGUS/GRVC/Datasets/ML/Bridges_or_cracks/multiclassifier/Splits/150/TIM" \
+		model.arch='resnet18' \
+		evaluate=True \
+		eval.method='tim_gd' \
+		eval.target_split_dir="/media/grvc/MAMG_ViGUS/GRVC/Datasets/ML/Bridges_or_cracks/multiclassifier/Splits/150/TIM" \
+		eval.checking=True \
+		eval.n_ways=5 \
+		eval.shots=[5] \
+		eval.query_shots=[568,127,257,13,234] \
+		eval.number_tasks=1 \
+		eval.fresh_start=True \
+		eval.inductive=True\
